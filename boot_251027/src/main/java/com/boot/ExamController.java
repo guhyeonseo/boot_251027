7c94802ec6309003625e49a3fb368ff16205714e
guhyeonseo <gumansi@naver.com>
2025-10-27 16:43:18 +0900

    666

--- boot_251027/src/main/java/com/boot/ExamController.java
+++ boot_251027/src/main/java/com/boot/ExamController.java
@@ -11,7 +11,7 @@ public class ExamController {
 	@RequestMapping("/")
 //	public void home() {
 	public String home() {
-		log.info("Hello boot 111 222 333 444 555");
+		log.info("Hello boot 111 222 333 444 666");
 		
 		return "hello";
 	}
