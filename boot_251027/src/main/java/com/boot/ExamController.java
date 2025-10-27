590abce5c9fd37b7f52d0c89e2c956f890572eaa
guhyeonseo <gumansi@naver.com>
2025-10-27 15:50:49 +0900

    최소 커밋

---
+++ boot_251027/src/main/java/com/boot/ExamController.java
@@ -0,0 +1,19 @@
+package com.boot;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import lombok.extern.slf4j.Slf4j;
+
+//777
+@Controller
+@Slf4j
+public class ExamController {
+	@RequestMapping("/")
+//	public void home() {
+	public String home() {
+		log.info("Hello boot");
+		
+		return "hello";
+	}
+}
